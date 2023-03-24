2896abf84c13d94cdd4adcec026721691e71b20c
Lee Webb <leewerbb55@hotmail.com>
2023-03-24 19:22:46 +0000

    First commit with some psuedocode

---
+++ assets/js/scores.js
@@ -0,0 +1,14 @@
+
+//Need a Button to start the quiz - what happens when the quiz starts?
+
+//use textContent on question-title to generate each question.
+
+//Use the array thing from the todos list to go through and generate divs with choices.
+
+//Implement some kind of select/onClick event listener when you choose an answer
+
+//Implement some kind of thing to tell you if you're right or wrong.
+
+//Score later?
+
+//When the game ends, hide the questions and show the end-screen message
